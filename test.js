a4b4fd7b21aba36ede7b7f6fd2ca95b4286aea4b
rkotenko <robkotenko@gmail.com>
2014-07-02 11:03:55 -0400

    Forward movement of quiz is complete

--- test.js
+++ test.js
@@ -1,12 +1,38 @@
 var i = 0;  // question count
-//document.getElementById('content').style.display = "none" // start off invisible before question is inserted
+document.getElementById('next').onclick = nextQuestion;
+document.getElementById('next').disabled = true;
+document.getElementById('results').style.display = 'none';  //hide the results div to start
 
-// insert the first set of question and answers into the page
-document.getElementById('question').innerHTML = allQuestions[i].question;
+// add an onchange event to the quiz div that holds the questions
+// Since the only thing that changes are the radio buttons, any change on the div mean
+// on was selected and the next button can be enabled
+document.getElementById('quiz').onchange = function(){
+	document.getElementById('next').disabled = false;
+};
 
-// only four possible answers so use 4 for the loop
-for(var j = 0; j < 4;j++){
-	document.getElementById(j + '_label').innerHTML = allQuestions[i].choices[j];
+// set up the initial question
+setUpQuestion();
+
+
+/*************** Functions ********************/
+
+function displayResults() {
+	// first hide the question div
+	document.getElementById('quiz').style.display = 'none';
+	
+	// compute the results
+	var total = 0;
+	var length = allQuestions.length;
+	
+	for(var j = 0; j < length; j++){
+		if(allQuestions[j].chosenAnswer == allQuestions[j].correctAnswer){
+			total++;
+		}
+	}
+	
+	document.getElementById('num_correct').innerHTML = total + ' out of ' + length + ' correct';
+	document.getElementById('percent').innerHTML = (total / length) * 100 + '%';		
+	document.getElementById('results').style.display = 'block';
 }
 
 function nextQuestion()
@@ -15,12 +41,33 @@ function nextQuestion()
 	for(var j = 0; j < 4;j++){
 		if(document.getElementById(j).checked) {
 			allQuestions[i].chosenAnswer = j;
+			
+			// uncheck the radio button to get ready for the next question
+			document.getElementById(j).checked = '';
 		}
+	}
+	
+	// check if the all questions have been used. If not, set up next one.
+	// if so, compute the number correct and show the total percentage
+	i++;
+	
+	if(i > allQuestions.length - 1) {
+		displayResults();
+	} else {
+		setUpQuestion();
 	}
 }
 
+function setUpQuestion(){
+	// insert the first set of question and answers into the page
+	document.getElementById('question').innerHTML = allQuestions[i].question;
 
-
-
-
+	// only four possible answers so use 4 for the loop
+	for(var j = 0; j < 4;j++){
+		document.getElementById(j + '_label').innerHTML = allQuestions[i].choices[j];
+	}
 	
+	// disable the next button.  Since the quiz only goes forward right now, no need to check if 
+	// the user answered or not
+	document.getElementById('next').disabled = true;
+}
