d542e023a1c642f3f1ac15852709a38493697f4d
rkotenko <robkotenko@gmail.com>
2014-07-02 18:25:07 -0400

    Quiz now allows back and forward movement

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
 var i = 0;  // question count
-document.getElementById('next').onclick = nextQuestion;
-document.getElementById('next').disabled = true;
-document.getElementById('results').style.display = 'none';  //hide the results div to start
-
-// add an onchange event to the quiz div that holds the questions
-// Since the only thing that changes are the radio buttons, any change on the div mean
-// on was selected and the next button can be enabled
-document.getElementById('quiz').onchange = function(){
-	document.getElementById('next').disabled = false;
-};
+doPrep();	// doPrep does all the initial work, applying events and hiding certain things
 
 // set up the initial question
 setUpQuestion();
@@ -35,8 +26,26 @@ function displayResults() {
 	document.getElementById('results').style.display = 'block';
 }
 
+function doPrep(){
+	document.getElementById('next').onclick = nextQuestion;
+	document.getElementById('next').disabled = true;
+	document.getElementById('results').style.display = 'none';  //hide the results div to start
+	document.getElementById('back').onclick = previousQuestion;
+	document.getElementById('back').style.display = 'none'; // hide the back button on first page
+
+	// add an onchange event to the quiz div that holds the questions
+	// Since the only thing that changes are the radio buttons, any change on the div mean
+	// on was selected and the next button can be enabled
+	document.getElementById('quiz').onchange = function(){
+		document.getElementById('next').disabled = false;
+	};
+}
+
 function nextQuestion()
 {
+	// reveal the back button.  Really should check if necessary but impact is minimal
+	document.getElementById('back').style.display = 'inline-block';
+	
 	// place the selected answer into the chosenAnswer field of the question
 	for(var j = 0; j < 4;j++){
 		if(document.getElementById(j).checked) {
@@ -46,6 +55,7 @@ function nextQuestion()
 			document.getElementById(j).checked = '';
 		}
 	}
+	
 	
 	// check if the all questions have been used. If not, set up next one.
 	// if so, compute the number correct and show the total percentage
@@ -58,8 +68,27 @@ function nextQuestion()
 	}
 }
 
+function previousQuestion(){
+	i--;  // remove one from the question count
+	
+	if(i == 0){
+		document.getElementById('back').style.display = 'none';
+	}
+	
+	setUpQuestion();
+}
+
 function setUpQuestion(){
-	// insert the first set of question and answers into the page
+	
+	// disable the next button if the question is unanswered. Make sure it is enabled
+	// if there was an answer given
+	if(!allQuestions[i].chosenAnswer){
+		document.getElementById('next').disabled = true;
+	}else {
+		document.getElementById('next').disabled = false;	
+	}
+	
+	// insert the question and answers into the page
 	document.getElementById('question').innerHTML = allQuestions[i].question;
 
 	// only four possible answers so use 4 for the loop
@@ -67,7 +96,8 @@ function setUpQuestion(){
 		document.getElementById(j + '_label').innerHTML = allQuestions[i].choices[j];
 	}
 	
-	// disable the next button.  Since the quiz only goes forward right now, no need to check if 
-	// the user answered or not
-	document.getElementById('next').disabled = true;
+	// if there is an answer present, check the proper radio button
+	if(allQuestions[i].chosenAnswer){
+		document.getElementById(allQuestions[i].chosenAnswer).checked = true
+	}
 }
