eebdf09471fecc187cd351dedcc79abd54099a43
rkotenko <robkotenko@gmail.com>
2014-07-02 21:11:37 -0400

    Converted code to use jquery
    Replaced getElementById with $, etc.  Added some fade effects for when
    questions change.

--- test.js
+++ test.js
@@ -1,103 +1,108 @@
-var i = 0;  // question count
-doPrep();	// doPrep does all the initial work, applying events and hiding certain things
+$('#document').ready(function() {
+	var i = 0;  // question count
+	doPrep();	// doPrep does all the initial work, applying events and hiding certain things
 
-// set up the initial question
-setUpQuestion();
-
-
-/*************** Functions ********************/
-
-function displayResults() {
-	// first hide the question div
-	document.getElementById('quiz').style.display = 'none';
-	
-	// compute the results
-	var total = 0;
-	var length = allQuestions.length;
-	
-	for(var j = 0; j < length; j++){
-		if(allQuestions[j].chosenAnswer == allQuestions[j].correctAnswer){
-			total++;
-		}
-	}
-	
-	document.getElementById('num_correct').innerHTML = total + ' out of ' + length + ' correct';
-	document.getElementById('percent').innerHTML = (total / length) * 100 + '%';		
-	document.getElementById('results').style.display = 'block';
-}
-
-function doPrep(){
-	document.getElementById('next').onclick = nextQuestion;
-	document.getElementById('next').disabled = true;
-	document.getElementById('results').style.display = 'none';  //hide the results div to start
-	document.getElementById('back').onclick = previousQuestion;
-	document.getElementById('back').style.display = 'none'; // hide the back button on first page
-
-	// add an onchange event to the quiz div that holds the questions
-	// Since the only thing that changes are the radio buttons, any change on the div mean
-	// on was selected and the next button can be enabled
-	document.getElementById('quiz').onchange = function(){
-		document.getElementById('next').disabled = false;
-	};
-}
-
-function nextQuestion()
-{
-	// reveal the back button.  Really should check if necessary but impact is minimal
-	document.getElementById('back').style.display = 'inline-block';
-	
-	// place the selected answer into the chosenAnswer field of the question
-	for(var j = 0; j < 4;j++){
-		if(document.getElementById(j).checked) {
-			allQuestions[i].chosenAnswer = j;
-			
-			// uncheck the radio button to get ready for the next question
-			document.getElementById(j).checked = '';
-		}
-	}
-	
-	
-	// check if the all questions have been used. If not, set up next one.
-	// if so, compute the number correct and show the total percentage
-	i++;
-	
-	if(i > allQuestions.length - 1) {
-		displayResults();
-	} else {
-		setUpQuestion();
-	}
-}
-
-function previousQuestion(){
-	i--;  // remove one from the question count
-	
-	if(i == 0){
-		document.getElementById('back').style.display = 'none';
-	}
-	
+	// set up the initial question
 	setUpQuestion();
-}
 
-function setUpQuestion(){
-	
-	// disable the next button if the question is unanswered. Make sure it is enabled
-	// if there was an answer given
-	if(!allQuestions[i].chosenAnswer){
-		document.getElementById('next').disabled = true;
-	}else {
-		document.getElementById('next').disabled = false;	
-	}
-	
-	// insert the question and answers into the page
-	document.getElementById('question').innerHTML = allQuestions[i].question;
 
-	// only four possible answers so use 4 for the loop
-	for(var j = 0; j < 4;j++){
-		document.getElementById(j + '_label').innerHTML = allQuestions[i].choices[j];
-	}
+	/*************** Functions ********************/
+
+	function displayResults() {
+		// first hide the question div
+		$('#quiz').css('display', 'none');
 	
-	// if there is an answer present, check the proper radio button
-	if(allQuestions[i].chosenAnswer){
-		document.getElementById(allQuestions[i].chosenAnswer).checked = true
+		// compute the results
+		var total = 0;
+		var length = allQuestions.length;
+	
+		for(var j = 0; j < length; j++){
+			if(allQuestions[j].chosenAnswer == allQuestions[j].correctAnswer){
+				total++;
+			}
+		}
+	
+		$('#num_correct').html(total + ' out of ' + length + ' correct');
+		$('#percent').html((total / length) * 100 + '%');		
+		$('#results').fadeIn('slow');
 	}
-}
+
+	function doPrep(){
+		$('#next').click(nextQuestion);
+		$('#next').prop('disabled', true);
+		$('#results').css('display', 'none');  //hide the results div to start
+		$('#back').click(previousQuestion);
+		$('#back').css('display', 'none'); // hide the back button on first page
+
+		// add an onchange event to the quiz div that holds the questions
+		// Since the only thing that changes are the radio buttons, any change on the div mean
+		// on was selected and the next button can be enabled
+		$('#quiz').change(function(){
+			$('#next').prop('disabled', false);
+		});
+	}
+
+	function nextQuestion()
+	{
+		// reveal the back button.  Really should check if necessary but impact is minimal
+		$('#back').css('display', 'inline-block');
+	
+		// place the selected answer into the chosenAnswer field of the question
+		for(var j = 0; j < 4;j++){
+			if($('#' + j).prop('checked')) {
+				allQuestions[i].chosenAnswer = j;
+			
+				// uncheck the radio button to get ready for the next question
+				$('#' + j).prop('checked', false);
+			}
+		}
+	
+	
+		// check if the all questions have been used. If not, set up next one.
+		// if so, compute the number correct and show the total percentage
+		i++;
+	
+		if(i > allQuestions.length - 1) {
+			$('#quiz').fadeOut('slow', displayResults);
+		} else {
+			$('#quiz').fadeOut('slow', setUpQuestion);
+		}
+	}
+
+	function previousQuestion(){
+		i--;  // remove one from the question count
+	
+		if(i == 0){
+			$('#back').css('display', 'none');
+		}
+		
+		// fade out the question div and set up the next question
+		$('#quiz').fadeOut('slow', setUpQuestion);
+		
+	}
+
+	function setUpQuestion(){
+	
+		answer = allQuestions[i].chosenAnswer;
+	
+		// disable the next button if the question is unanswered. Make sure it is enabled
+		// if there was an answer given
+		// also mark a radio button as checked if the answer is present
+		if(answer == null){
+			$('#next').prop('disabled', true);
+		}else {
+			$('#next').prop('disabled', false);
+			$('#' + answer).prop('checked', true);	
+		}
+	
+		// insert the question and answers into the page
+		$('#question').html(allQuestions[i].question);
+
+		// only four possible answers so use 4 for the loop
+		for(var j = 0; j < 4;j++){
+			$('#' + j + '_label').html(allQuestions[i].choices[j]);
+		}
+		
+		$('#quiz').fadeIn('slow');
+	}
+});
